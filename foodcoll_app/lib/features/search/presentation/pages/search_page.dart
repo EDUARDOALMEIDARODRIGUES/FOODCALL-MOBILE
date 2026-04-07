fdc79aab1e560cb19fecdfcc12ecaf27f81826ae
Rhafaely Ananda Costa dos Reis <rhafaelyreis@gmail.com>
2026-04-07 14:43:50 -0400

    ✨feat(search): criar estrutura base da tela de busca
    - Adicionada SearchPage com BasePage
    - Inserido campo de busca (SearchInput)
    - Inserido botão de sugestão
    - Estrutura inicial da UI sem integração com dados

--- foodcoll_app/lib/features/search/presentation/pages/search_page.dart
+++ foodcoll_app/lib/features/search/presentation/pages/search_page.dart
@@ -1,15 +1,32 @@
 import 'package:flutter/material.dart';
 import '../../../../core/base_page.dart';
+import '../widgets/search_input.dart';
+import '../widgets/suggestion_button.dart';
 
 class SearchPage extends StatelessWidget {
   const SearchPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const BasePage(
+    return BasePage(
       title: 'Buscar',
       currentIndex: 1,
-      body: SizedBox.shrink(),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          SearchInput(
+            onChanged: (value) {
+              debugPrint(value);
+            },
+          ),
+          SuggestionButton(
+            onPressed: () {
+              print("Botão de sugestão pressionado");
+            },
+          ),
+        ],
+      ),
     );
   }
 }
