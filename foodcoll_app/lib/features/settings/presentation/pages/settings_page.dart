56d5fb9b5728109c5663c7ecd3c03823f51e0379
Bozzi_afk <pedroh.bossi@gmail.com>
2026-04-10 16:12:03 -0400

    time

--- foodcoll_app/lib/features/settings/presentation/pages/settings_page.dart
+++ foodcoll_app/lib/features/settings/presentation/pages/settings_page.dart
@@ -1,15 +1,77 @@
 import 'package:flutter/material.dart';
+import 'package:foodcoll_app/shared/widgets/page_header.dart';
 import '../../../../core/base_page.dart';
 
 class SettingsPage extends StatelessWidget {
   const SettingsPage({super.key});
+  
 
   @override
   Widget build(BuildContext context) {
-    return const BasePage(
+    return  BasePage( 
+      //tirei o const, tava dando erro no onBack
       title: 'Configurações',
       currentIndex: 3,
-      body: SizedBox.shrink(),
+      body: Column(
+        children: [
+          PageHeader(
+            title: "Configurações",
+            onBack: () => true,
+            ),
+            ListView(
+              shrinkWrap: true,
+              physics: NeverScrollableScrollPhysics(),
+              children: [
+                SwitchListTile(
+                  secondary: Icon(Icons.dark_mode,color: Color(0xFF1FA7A6)),
+                  title: Center(
+                    child: Text("Modo Escuro")
+                    ),
+                  value: false,
+                  onChanged: (bool value) => {},
+                  inactiveTrackColor: Color(0xFFA8D5D4),
+                  inactiveThumbColor: Color(0xFFFFFFFF),
+                  activeThumbColor: Color(0xFFFFFFFF),
+                  activeTrackColor: Color(0xFFA8D5D4),
+                  trackOutlineColor: WidgetStateProperty.all(Colors.transparent),
+                ),
+                Divider( 
+                  color: Color(0xFFA8D5D4),
+                  thickness: 2
+                  ),
+                ListTile(
+                  leading: Icon(Icons.format_size,color: Color(0xFF1FA7A6),),
+                  title: Center(
+                    child: Text("Tamanho da fonte")),
+                  trailing: DropdownButton<String>(
+                    value: "P", 
+                    items: [], 
+                    onChanged:(value){}),
+                ),
+                Divider(
+                  color: Color(0xFFA8D5D4),
+                  thickness: 2,    
+                ),
+                ListTile(
+                  leading: Icon(Icons.info,color: Color(0xFF1FA7A6),),
+                  title: Center(
+                    child: Text("Sobre o App"),
+                  )
+                ),
+                Divider(
+                  color: Color(0xFFA8D5D4),
+                  thickness: 2
+                ),
+                ListTile(
+                  leading: Icon(Icons.logout,color: Color(0xFF1FA7A6),),
+                  title: Center(
+                    child: Text("Sair"),
+                  )
+                ),
+              ],
+            )
+        ],
+      ),
     );
   }
 }
