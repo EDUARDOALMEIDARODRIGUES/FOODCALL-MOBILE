201935c917101e39363de311aaafca4dc62e81a8
Eduardo Almeida Rodrigues <eduaralmeidarodri@gmail.com>
2026-04-26 18:27:52 -0400

    🐛fix: correção de importação do .env

--- back-end/src/config/server.js
+++ back-end/src/config/server.js
@@ -1,9 +1,12 @@
+//importação
 import { app }from "../app.js";
-
-require('dotenv').config();
-
+import dotenv from "dotenv";
+//conecção com o .env
+dotenv.config()
+//atribuindo valor as constantes
 const port = process.env.PORTA;
 
+//roda o servidor deacordo com a porta
 app.listen(port, () => {
   console.log(`Servidor rodando em http://localhost:${port}`);
 });
