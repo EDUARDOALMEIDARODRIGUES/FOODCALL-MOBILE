81280d700213c279220cf81e38567ec16d53e2bf
Rhafaely Ananda Costa dos Reis <rhafaelyreis@gmail.com>
2026-05-04 19:34:04 -0400

    ♻️refactor: simplificar main.dart e reduzir boilerplate

--- foodcoll_app/lib/main.dart
+++ foodcoll_app/lib/main.dart
@@ -1,13 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:foodcoll_app/features/collocations/presentation/pages/collocations_page.dart';
+import 'features/collocations/presentation/pages/collocations_page.dart';
 import 'features/home/presentation/pages/home_page.dart';
 import 'features/search/presentation/pages/search_page.dart';
 import 'features/favorites/presentation/pages/favorites_page.dart';
 import 'features/settings/presentation/pages/settings_page.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -17,13 +15,37 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       initialRoute: '/home',
+      theme: ThemeData(
+        pageTransitionsTheme: PageTransitionsTheme(
+          builders: {
+            for (final platform in TargetPlatform.values)
+              platform: const _FadeTransitionsBuilder(),
+          },
+        ),
+      ),
       routes: {
         '/home': (_) => const HomePage(),
         '/search': (_) => const SearchPage(),
         '/favorites': (_) => const FavoritesPage(),
         '/settings': (_) => const SettingsPage(),
-        '/collocations': (_) => const CollocationsPage(),
+        '/collocations': (context) => CollocationsPage(
+              collocation: ModalRoute.of(context)!.settings.arguments as String,
+            ),
       },
     );
   }
+}
+
+class _FadeTransitionsBuilder extends PageTransitionsBuilder {
+  const _FadeTransitionsBuilder();
+
+  @override
+  Widget buildTransitions<T>(
+    PageRoute<T> route,
+    BuildContext context,
+    Animation<double> animation,
+    Animation<double> secondaryAnimation,
+    Widget child,
+  ) =>
+      FadeTransition(opacity: animation, child: child);
 }
