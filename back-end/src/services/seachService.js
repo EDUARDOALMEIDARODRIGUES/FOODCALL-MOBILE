a82f8c28cc6b4f69fccb5d5e3c24b0ebc0654230
Eduardo Almeida Rodrigues <eduaralmeidarodri@gmail.com>
2026-05-05 19:26:23 -0400

    ✨feat: Adicionado a funçao de armazenar no historico a busca
    foi adicionado a função, do serviço de historico, de armazenar a busca do usuario, podendo ser qualquer coisa, menos vazia já que a busca vazia returna um resposta antes de executar o armazenamento do historico

--- back-end/src/services/seachService.js
+++ back-end/src/services/seachService.js
@@ -1,8 +1,15 @@
-import { mostrarHistorico,pesquisaDBC } from "../models/seachModel.js";
+import { pesquisaDBC } from "../models/seachModel.js";
+import { insererirHistoricoDB } from "../models/historicoModel.js";
+import { mostrarHistorico } from "../models/historicoModel.js";
+import { conecta, fecha } from "../config/db.js";
 
 export async function validaPesquisa(query) {
+  conecta();
   if (!query) return mostrarHistorico();
-  console.log("oia eu  no service");
 
-  return pesquisaDBC(query.toLowerCase());
+  console.log("oia eu  no service");
+  await insererirHistoricoDB(query.toLowerCase());
+  const resultado = await pesquisaDBC(query.toLowerCase());
+  fecha();
+  return resultado;
 }
