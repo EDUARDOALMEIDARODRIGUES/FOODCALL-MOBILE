c8b5524fa04374fa71ae9d28381a86005ca071c2
Eduardo Almeida Rodrigues <eduaralmeidarodri@gmail.com>
2026-05-05 19:35:21 -0400

    🐛fix: correção de erro de importação

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -1,7 +1,7 @@
 import express from "express";
 import cors from "cors";
-import { router } from "./routes/seachRoutes.js";
-import { errosNaExecucao } from "./middlewares/seachMiddleware.js";
+import { router } from "./routes/Routes.js";
+import { errosNaExecucao } from "./middlewares/pesquisaMiddleware.js";
 
 //constantes
 const app = express();
