48ebcc2375eaeb5c6f2077855d32c78379968f46
Eduardo Almeida Rodrigues <eduaralmeidarodri@gmail.com>
2026-05-05 19:23:44 -0400

    ✨feat: Adicionado a rota para o serviço de favoritos
    Adcionei a rota do serviço de favoritos e mudei o nome do arquivo para apenas router

--- back-end/src/routes/Routes.js
+++ back-end/src/routes/Routes.js
@@ -1,8 +1,10 @@
 import express from "express";
 import { pesquisa } from "../controllers/seachController.js";
+import { favorito } from "../controllers/favoritoController.js";
 
 const router= express.Router();
 
 router.get('/search', pesquisa);
+router.get('/favorito/:ID', favorito)
 
 export { router };
